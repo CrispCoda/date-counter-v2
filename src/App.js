b8e0b5aa81a07a0084b5fb8785eaf099a333ac9f
CrispCoda <joshua.p.crisp@outlook.com>
2024-07-23 15:26:14 +0100

    V2 Counter with Slider + Controlled Elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
   );
 }
+
+const Counter = () => {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="counter">
+      <div>
+        <div>Step: {step}</div>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+      </div>
+      <div>
+        <button onClick={() => setCount(count - step)}>-</button>
+        <input
+          type="number"
+          className="countbar"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount(count + step)}>+</button>
+      </div>
+
+      <div className="date">
+        <span>{count === 0 ? "Today is " : ""}</span>
+        <span>{count >= 1 ? `${count} days from today is ` : ""}</span>
+        <span>{count < 0 ? `${Math.abs(count)} days ago was ` : ""}</span>
+        <span>{date.toDateString()}</span>
+      </div>
+    </div>
+  );
+};
 
 export default App;
